27d034d1f56eefde30d3ce2e8e09c8fe584dc5ec
shou0224 <sugawarashou2@gmail.com>
2023-04-25 10:58:28 +0900

    ゲームクリアを設定

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const parent=document.getElementById("parent");
 const button=document.getElementById("button");
 const img=document.getElementById("img");
+const beanNum=document.createElement("div");
+let beanCount=0;
+let bool=false;
 
 //START
 Start();
@@ -15,7 +18,8 @@ function Start()
 setInterval(Update,1000/60);
 function Update()
 {
-
+  beanNum.textContent="豆"+beanCount;
+  GameClear();
 }
 
 //
@@ -24,6 +28,7 @@ function Init()
   button.addEventListener('click',function(){
     img.src="open_pack.png";
     img.style.zIndex=100;
+    document.body.appendChild(beanNum);
 
     PopBean();
   })
@@ -40,6 +45,7 @@ function PopBean()
 
   function CreateBean(top,left)
   {
+    beanCount++;
     const bean=document.createElement("img");
     parent.appendChild(bean);
     bean.src="bean.png";
@@ -47,7 +53,26 @@ function PopBean()
     bean.style.top=top+"px";
     bean.style.left=left+"px";
     bean.style.zIndex=200;
+    bean.addEventListener('click',function(){
+      this.remove();
+      beanCount--;
+      bool=true;
+    })
 
+  }
+
+}
+
+//
+function GameClear()
+{
+  if(bool && beanCount==0)
+  {
+    const gameClear=document.createElement("div");
+    parent.appendChild(gameClear);
+    gameClear.textContent="GAME CLEAR";
+    gameClear.style.zIndex=1000;
+    bool=false;
   }
 
 }
