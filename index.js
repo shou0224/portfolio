9a4ef168b0456fa7e2e5f7bcfdb06e16cfaf06cf
shou0224 <sugawarashou2@gmail.com>
2023-04-25 10:24:48 +0900

    豆を表示

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+
+const parent=document.getElementById("parent");
 const button=document.getElementById("button");
 const img=document.getElementById("img");
 
@@ -21,5 +24,20 @@ function Init()
   button.addEventListener('click',function(){
     img.src="open_pack.png";
     img.style.zIndex=100;
+
+    PopBean();
   })
+}
+
+//
+function PopBean()
+{
+  const bean=document.createElement("img");
+  parent.appendChild(bean);
+  bean.src="bean.png";
+  bean.style.position="absolute";
+  bean.style.top=100+"px";
+  bean.style.left=100+"px";
+  bean.style.zIndex=200;
+
 }
