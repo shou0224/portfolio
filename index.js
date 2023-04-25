0c544aac734fd7abbeef6522e0725bdcab46f64e
shou0224 <sugawarashou2@gmail.com>
2023-04-25 10:05:50 +0900

    start update追加

---
+++ index.js
@@ -0,0 +1,16 @@
+const button=document.getElementById("button");
+const img=document.getElementById("img");
+
+//START
+Start();
+function Start()
+{
+
+}
+
+//UPDATE
+setInterval(Update,1000/60);
+function Update()
+{
+  
+}
